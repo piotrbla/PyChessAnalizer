9145811cbf560093341eea946547934eaa63ef45
PiotrB <piotrb@gmail.com>
2018-05-03 16:54:29 +0200

    Added empty classes for pieces

--- pieces.py
+++ pieces.py
@@ -5,6 +5,31 @@ class Piece:
         self.board_info = board_info
 
     def draw(self, canvas, position_x, position_y):
+        self.board_info.field_size_x = 0
+
+
+class King(Piece):
+    def draw(self, canvas, position_x, position_y):
+        pass
+
+
+class Queen(Piece):
+    def draw(self, canvas, position_x, position_y):
+        pass
+
+
+class Bishop(Piece):
+    def draw(self, canvas, position_x, position_y):
+        pass
+
+
+class Rook(Piece):
+    def draw(self, canvas, position_x, position_y):
+        pass
+
+
+class Knight(Piece):
+    def draw(self, canvas, position_x, position_y):
         pass
 
 
@@ -12,6 +37,5 @@ class Pawn(Piece):
     def draw(self, canvas, position_x, position_y):
         canvas.create_rectangle(position_x,
                                 position_y,
-                                position_x + self.board_info.field_size_x-20,
-                                position_y+self.board_info.field_size_y-20, fill="cornsilk2")
-
+                                position_x + self.board_info.field_size_x - 20,
+                                position_y + self.board_info.field_size_y - 20, fill="cornsilk2")
